af4c82449c07547e3308663e92d8d94fccbec760
mulf <mhmulford0@gmail.com>
2024-06-05 13:31:27 -0500

    checkpoint

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,17 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+
+  {
+    'prettier/vim-prettier',
+    run = 'yarn install',
+    config = function()
+      vim.g['prettier#config#config_precedence'] = 'prefer-file'
+      vim.g['prettier#exec_cmd_path'] = 'prettier'
+      vim.g['prettier#autoformat'] = 1
+      vim.g['prettier#autoformat_require_pragma'] = 0
+      vim.g['prettier#quickfix_enabled'] = 1
+    end,
+  },
+}
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -3,7 +3,6 @@
 
 return {
   'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
@@ -11,15 +10,6 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
+    { '<leader>e', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
   },
 }
